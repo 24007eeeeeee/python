7c4c4c89d267580f810fdd275f75be416f16cbc2
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-15 14:02:43 +1200

    commit2

--- app1.py
+++ app1.py
@@ -1,3 +1,9 @@
 import sqlite3
 
 db = sqlite3.connect("SWfilms.db")
+cursor = db.cursor()
+sql = "SELECT * from SWfilms;"
+cursor.execute(sql)
+results = cursor.fetchall()
+print(results)
+db.close()
