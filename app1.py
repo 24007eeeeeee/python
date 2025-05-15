c61e02587e5752f27b71f8b00bbddf3325573bb3
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-15 14:40:07 +1200

    COMMIT5SAVE

--- app1.py
+++ app1.py
@@ -1,9 +1,22 @@
+# docstring- Chris Wong- Star Wars database application
+# imports
 import sqlite3
 
-db = sqlite3.connect("SWfilms.db")
-cursor = db.cursor()
-sql = "SELECT * from movie;"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(results)
-db.close()
+# contants and variables
+DATABASE = "SWfilms.db"
+
+
+# functions
+def print_all_movie():
+    '''print all the movies nicely'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * from movie;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # loop through all the results
+    print(results)
+    db.close
+
+# main code
+print_all_movie()
