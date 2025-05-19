0cd42eed56d9dae333aff10ce69efa4aab4c01b6
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-19 14:17:35 +1200

    19/05/2025

--- app1.py
+++ app1.py
@@ -15,6 +15,7 @@ def print_all_movie():
     cursor.execute(sql)
     results = cursor.fetchall()
     # loop through all the results
+    print(f"id                            movie_name  director_id  duration_in_minutes  rating  box_office")
     for movie in results:
         print(f"{movie[1]:<30}{movie[2]:<8}{movie[3]:<6}{movie[4]:<6}{movie[5]:<6}")
     # loop finished here
