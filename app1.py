b4648d461cd98f56422b7385a2a19998520c55f3
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-20 12:32:44 +1200

    Finished Code

--- app1.py
+++ app1.py
@@ -21,18 +21,34 @@ def print_all_movie():
     # loop finished here
     db.close
 
+
 def print_all_movie_by_duration():
     '''print all the movies sorted by duration'''
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
-    sql = "SELECT * FROM movie ORDER BY duration_in_minutes; DESC;"
+    sql = "SELECT * FROM movie ORDER BY duration_in_minutes DESC;"
     cursor.execute(sql)
     results = cursor.fetchall()
-    #loop through all the results
+    # loop through all the results
     print("|movie_name                   |director_id |duration_in_minutes |rating |box_office ")
     for movie in results:
         print(f"|{movie[1]:<30}{movie[2]:<13}{movie[3]:<21}{movie[4]:<8}{movie[5]:<25}")
-    #loop finished here
+    # loop finished here
+    db.close
+
+
+def print_all_movie_sorted_by_box_office():
+    '''print all the movies sorted by box office'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = " SELECT * FROM movie ORDER BY box_office;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # loop through all the results
+    print("|movie_name                   |director_id |duration_in_minutes |rating |box_office ")
+    for movie in results:
+        print(f"|{movie[1]:<30}{movie[2]:<13}{movie[3]:<21}{movie[4]:<8}{movie[5]:<25}")
+    # loop finished here
     db.close
 
 
@@ -43,21 +59,16 @@ while True:
 What would you like to do.
 1. Print all movies
 2. Print all movies sorted by duration
-3. Print all movies sorted by box_office
-7. Exit
+3. Print all movies sorted by box office
+4. Exit
 """)
     if user_input == "1":
         print_all_movie()
     elif user_input == "2":
-        print_all_movie_by_duration
-        
-
-
-
-
-while True:
-    user_input = input("\nWhat would you like to do.\n1. Print all movies.\n2. Sort movie order by duration\n3. Sort movie order by Box_office")
-    if user_input == "1":
-        print_all_movie()
-    elif user_input == "2":
-        print_all_movie_by_duration
+        print_all_movie_by_duration()
+    elif user_input == "3":
+        print_all_movie_sorted_by_box_office()
+    elif user_input == "4":
+        break
+    else:
+        print("That was not an option\n")
